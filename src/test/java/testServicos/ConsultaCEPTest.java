92517922231bc3cc1e62611d570fb437538a8157
Adam Vinicius <adamcursogit@gmail.com>
2020-09-10 09:01:27 -0300

    inicio get frame

--- src/test/java/testServicos/ConsultaCEPTest.java
+++ src/test/java/testServicos/ConsultaCEPTest.java
@@ -1,9 +1,16 @@
 package testServicos;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedHashMap;
+
 import org.junit.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class ConsultaCEPTest {
 	String url = "http://viacep.com.br/ws/";
@@ -16,13 +23,83 @@ public class ConsultaCEPTest {
 		
 		RestAssured.baseURI = url;
 		
-		RestAssured.given()
-		.relaxedHTTPSValidation()
-		.contentType(ContentType.JSON)
-		.given()
+		Response response = get(endpoint);
+		assertEquals(200, response.statusCode());
+		
+	}
+	
+	@Test
+	public void validaDadosCEP() {
+		String cep = "04055041";
+		String endpoint = cep.concat("/json/");
+		LinkedHashMap<String, String> header = new LinkedHashMap<>();
+		header.put("client-id", "curso");
+		header.put("Authorization", "Basic YWRhbTp0ZXN0ZQ==");
+		
+		RestAssured.baseURI = url;
+		
+		RequestSpecification request = initRequest(ContentType.JSON, header);
+		Response response = request
+		.when()
 		.get(endpoint)
 		.then()
-		.statusCode(200);
+		.statusCode(200)
+		.extract()
+		.response();
+		
+		JsonPath json = response.getBody().jsonPath();
+		
+		assertEquals("Rua Mauro", json.get("logradouro"));
+		assertEquals("Saúde", json.get("bairro"));
+		assertEquals("São Paulo", json.get("localidade"));
+	}
+	
+	@Test
+	public void validaDadosCEPParam() {
+		String cep = "04055041";
+		String endpoint = cep.concat("/json/");
+		LinkedHashMap<String, String> param = new LinkedHashMap<>();
+		param.put("client-id", "curso");
+		param.put("nome", "Adam");
+		
+		RestAssured.baseURI = url;
+		
+		Response response = initRequest(ContentType.JSON)
+		.params(param)
+		.when()
+		.get(endpoint)
+		.then()
+		.statusCode(200)
+		.extract()
+		.response();
+		
+		JsonPath json = response.getBody().jsonPath();
+		
+		assertEquals("Rua Mauro", json.get("logradouro"));
+		assertEquals("Saúde", json.get("bairro"));
+		assertEquals("São Paulo", json.get("localidade"));
+	}
+	
+	public RequestSpecification initRequest(ContentType contentType, LinkedHashMap<String, String> header) {
+		return RestAssured.given()
+		.relaxedHTTPSValidation()
+		.contentType(contentType)
+		.headers(header);
+	}
+	
+	public RequestSpecification initRequest(ContentType contentType) {
+		return RestAssured.given()
+		.relaxedHTTPSValidation()
+		.contentType(contentType);
+	}
+	
+	public Response get(String endpoint) {
+		return initRequest(ContentType.JSON)
+		.when()
+		.get(endpoint)
+		.then()
+		.extract()
+		.response();
 	}
 	
 }
